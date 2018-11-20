2e7d5e53ba347c2f3d42ef142706f8c0737b5b65
dbmarch <dbmarch@gmail.com>
2018-11-19 19:58:05 -0600

    Animations.

--- v14-Animations/src/App.vue
+++ v14-Animations/src/App.vue
@@ -4,16 +4,41 @@
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Animations</h1>
                 <hr>
+                <select v-model="alertAnimation" class="form-control">
+                    <option value="fade">Fade</option>
+                    <option value="slide">Slide</option>
+                </select>
+                <hr>
                 <button class="btn btn-primary" @click="show=!show">Show Alert</button>
                 <br>
-                <br>
+                <!-- <br>
                 <transition name="fade">
                     <div class="alert alert-info" v-if="show">This is some info</div>
                 </transition>
-                <br>
-                <br>
-                <transition name="slide">
+                <transition name="slide" type="animation">
                     <div class="alert alert-info" v-if="show">This is more info</div>
+                </transition>
+                <br>-->
+
+                <br>
+                <transition :name="alertAnimation">
+                    <div class="alert alert-info" v-show="show">This is some info</div>
+                </transition>
+                <transition name="slide" type="animation" appear>
+                    <div class="alert alert-info" v-show="show">This is more info</div>
+                </transition>
+                <br>
+                <transition
+                    enter-active-class="animated bounce"
+                    leave-active-class="animated shake"
+                >
+                    <div class="alert alert-info" v-if="show">This is some info</div>
+                </transition>
+                <br>
+                <hr>
+                <transition :name="alertAnimation" mode="out-in">
+                    <div class="alert alert-info" v-if="show" key="info">This is some info</div>
+                    <div class="alert alert-warning" v-else key="warning">This is some warning</div>
                 </transition>
             </div>
         </div>
@@ -24,6 +49,7 @@
     export default {
         data() {
             return {
+                alertAnimation: "",
                 show: false
             }
         }
@@ -46,10 +72,12 @@
 }
 
 .slide-enter {
+	opacity: 0;
 }
 
 .slide-enter-active {
 	animation: slide-in 1s ease-out forwards;
+	transition: opacity 0.5s;
 }
 
 .slide-leave {
@@ -57,6 +85,8 @@
 
 .slide-leave-active {
 	animation: slide-out 1s ease-out forwards;
+	transition: opacity 1s;
+	opacity: 0;
 }
 
 @keyframes slide-in {
