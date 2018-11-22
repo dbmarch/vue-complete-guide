c7f4a6daaf20a4934c4804ebcbdf9c6ea2663926
dbmarch <dbmarch@gmail.com>
2018-11-21 23:42:39 -0600

    named routes, route animations, hash routes

--- v16-routing/src/components/user/UserDetail.vue
+++ v16-routing/src/components/user/UserDetail.vue
@@ -3,6 +3,11 @@
         <h3>Some User Details</h3>
         <p>User loaded id: {{$route.params.id}}</p>
         <router-link tag="button" :to="'/user/'+$route.params.id + '/edit'">Edit User</router-link>
+        <router-link
+            tag="button"
+            :to="{name: 'userEdit', params: { id: $route.params.id}, query: {locale: 'en', q: 100}}"
+            class="btn-btn-primary"
+        >Edit User</router-link>
     </div>
 </template>
 
--- v16-routing/src/routes.js
+++ v16-routing/src/routes.js
@@ -3,16 +3,29 @@ import UserStart from './components/user/UserStart.vue';
 import UserDetail from './components/user/UserDetail.vue';
 import UserEdit from './components/user/UserEdit.vue';
 import Home from './components/Home.vue';
+import Header from './components/Header.vue';
 
 export const routes = [
-	{ path: '', component: Home },
+	{
+		path: '',
+		name: 'home',
+		components: {
+			default: Home,
+			'header-top': Header,
+		},
+	},
 	{
 		path: '/user',
-		component: User,
+		components: {
+			default: User,
+			'header-bottom': Header,
+		},
 		children: [
 			{ path: '', component: UserStart },
 			{ path: ':id', component: UserDetail },
-			{ path: ':id/edit', component: UserEdit },
+			{ path: ':id/edit', component: UserEdit, name: 'userEdit' },
 		],
 	},
+	{ path: '/redirect-me', redirect: '/user' },
+	{ path: '*', redirect: '/' },
 ];
