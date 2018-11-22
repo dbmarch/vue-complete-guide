505593a83421cdc51c2871300d2adb07c10cbb12
dbmarch <dbmarch@gmail.com>
2018-11-21 23:10:39 -0600

    Vue Router module

---
+++ v16-routing/src/components/user/UserDetail.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <h3>Some User Details</h3>
+        <p>User loaded id: {{$route.params.id}}</p>
+        <router-link tag="button" :to="'/user/'+$route.params.id + '/edit'">Edit User</router-link>
+    </div>
+</template>
+
