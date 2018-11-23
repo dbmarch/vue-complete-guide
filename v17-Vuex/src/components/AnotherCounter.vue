6826a7f6e5c5e17944a0042c7fc8d14e8be0131a
dbmarch <dbmarch@gmail.com>
2018-11-22 19:48:29 -0600

    Getters and Mutations\ need babel-stage-2 for helper functions

---
+++ v17-Vuex/src/components/AnotherCounter.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <button class="btn btn-primary" @click="increment">Increment</button>
+        <button class="btn btn-primary" @click="decrement">Decrement</button>
+    </div>
+</template>
+
+<script>
+import {mapMutations} from 'vuex'
+
+    export default {
+        methods: {
+            ...mapMutations(['increment', 'decrement']),
+            myIncrement() {
+                // this.$emit('updated', 1);
+                this.$store.commit('increment')
+            },
+            myDecrement() {
+                // this.$emit('updated', -1);
+                this.$store.commit('decrement')
+            }
+        }
+    }
+</script>  
---
+++ v17-Vuex/src/components/AnotherResult.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <hr>
+        <p>Double Counter is: {{ counter }}</p>
+        <p>Number of Clicks: {{ clicks }}</p>
+    </div>
+</template>
+
+<script>
+    import {mapGetters} from 'vuex'
+    export default {
+
+        // either pass an array of strings or an objct with identifers mapped to the getter names.
+        computed: {
+            anotherValue() {
+
+            },
+            ...mapGetters({  // This needs babel-stage-2 installed.
+                counter: 'doubleCounter',
+                clicks: 'stringCounter'
+        })
+        }
+        // props: ['counter']
+        // computed: {
+        //     counter() {
+        //         return this.$store.getters.doubleCounter;
+        //     },
+        //     clicks() {
+        //         return this.$store.getters.stringCounter;
+        //     }
+        // }
+    }
+</script>
