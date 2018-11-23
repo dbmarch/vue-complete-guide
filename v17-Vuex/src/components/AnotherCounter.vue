17b657e0b798ac2e389c81bc9799336678820c0b
dbmarch <dbmarch@gmail.com>
2018-11-23 00:09:51 -0600

    vuex actions

--- v17-Vuex/src/components/AnotherCounter.vue
+++ v17-Vuex/src/components/AnotherCounter.vue
@@ -1,16 +1,32 @@
 <template>
     <div>
-        <button class="btn btn-primary" @click="increment">Increment</button>
-        <button class="btn btn-primary" @click="decrement">Decrement</button>
+        <hr>
+        <br>
+        <h2>Mutations</h2>
+        <button class="btn btn-primary" @click="increment(1)">Increment</button>
+        <button class="btn btn-primary" @click="decrement(1)">Decrement</button>
+        <hr>
+        <br>
+        <h2>Async Actions</h2>
+        <button
+            class="btn btn-primary"
+            @click="asyncIncrement({by: 50, duration:500})"
+        >Async Increment</button>
+        <button
+            class="btn btn-primary"
+            @click="asyncDecrement({by: 150, duration:2500})"
+        >Async Decrement</button>
     </div>
 </template>
 
 <script>
 import {mapMutations} from 'vuex'
+import {mapActions } from 'vuex'
 
     export default {
         methods: {
             ...mapMutations(['increment', 'decrement']),
+            ...mapActions (['asyncIncrement', 'asyncDecrement']),   // calls this.$store.dispatch ('action', payload)
             myIncrement() {
                 // this.$emit('updated', 1);
                 this.$store.commit('increment')
