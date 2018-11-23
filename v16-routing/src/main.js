afe45164557c20ce555c445c78f34429281d9b64
dbmarch <dbmarch@gmail.com>
2018-11-22 18:21:58 -0600

    route lifecycles and lazy loading

--- v16-routing/src/main.js
+++ v16-routing/src/main.js
@@ -8,7 +8,22 @@ Vue.use(VueRouter);
 const router = new VueRouter({
 	routes: routes,
 	mode: 'history',
+	scrollBehavior(to, from, savedPosition) {
+		if (savedPosition) {
+			return savedPosition;
+		}
+		if (to.hash) {
+			return { selector: to.hash };
+		}
+		// return { x: 0, y: 700 };
+	},
 });
+
+router.beforeEach((to, from, next) => {
+	console.log('global beforeEach', to);
+	next();
+});
+
 new Vue({
 	el: '#app',
 	router: router,
--- v16-routing/src/routes.js
+++ v16-routing/src/routes.js
@@ -1,9 +1,49 @@
-import User from './components/user/User.vue';
-import UserStart from './components/user/UserStart.vue';
-import UserDetail from './components/user/UserDetail.vue';
-import UserEdit from './components/user/UserEdit.vue';
+// import User from './components/user/User.vue';
+// import UserStart from './components/user/UserStart.vue';
+// import UserDetail from './components/user/UserDetail.vue';
+// import UserEdit from './components/user/UserEdit.vue';
 import Home from './components/Home.vue';
 import Header from './components/Header.vue';
+
+const User = resolve => {
+	require.ensure(
+		['./components/user/User.vue'],
+		() => {
+			resolve(require('./components/user/User.vue'));
+		},
+		'user'
+	);
+};
+
+const UserStart = resolve => {
+	require.ensure(
+		['./components/user/UserStart.vue'],
+		() => {
+			resolve(require('./components/user/UserStart.vue'));
+		},
+		'user'
+	);
+};
+
+const UserEdit = resolve => {
+	require.ensure(
+		['./components/user/UserEdit.vue'],
+		() => {
+			resolve(require('./components/user/UserEdit.vue'));
+		},
+		'user'
+	);
+};
+
+const UserDetail = resolve => {
+	require.ensure(
+		['./components/user/UserDetail.vue'],
+		() => {
+			resolve(require('./components/user/UserDetail.vue'));
+		},
+		'user'
+	);
+};
 
 export const routes = [
 	{
@@ -22,7 +62,14 @@ export const routes = [
 		},
 		children: [
 			{ path: '', component: UserStart },
-			{ path: ':id', component: UserDetail },
+			{
+				path: ':id',
+				component: UserDetail,
+				beforeEnter: (to, from, next) => {
+					console.log('inside route setup');
+					next();
+				},
+			},
 			{ path: ':id/edit', component: UserEdit, name: 'userEdit' },
 		],
 	},
